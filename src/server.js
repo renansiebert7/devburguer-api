632b22c3fedb9116dc25caaa07868eb4b00083d3
renansiebert7 <renansiebert01@gmail.com>
2026-06-05 18:02:22 -0300

    alterações server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import app from './app.js';
 import './database/index.js';
 
-app.listen(3001, () => {
-    console.log('Server running on port 3001');
-});
+const PORT = process.env.PORT || 3001;
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
